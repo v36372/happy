9a9318e20c61aca8c718872b7a846215b7485048
Tin Nguyen <tin@floatingcube.com>
2017-10-10 09:28:50 -0700

    Fix music player logic

--- cmd/happyWeb/index.go
+++ cmd/happyWeb/index.go
@@ -10,23 +10,23 @@ const songPerBatch = 20
 
 func (a *App) IndexHandler(db *happy.PGDB) HandlerWithError {
 	return func(w http.ResponseWriter, req *http.Request) error {
-		songList, err := db.GetSongList(songPerBatch)
-		if err != nil {
-			a.logr.Log("error when getting song list: %s", err)
-		}
+		// songList, err := db.GetSongList(songPerBatch)
+		// if err != nil {
+			// a.logr.Log("error when getting song list: %s", err)
+		// }
 
 		fs := a.getFlashes(w, req)
 
 		pp := struct {
 			*localPresenter
-			SongList []*happy.Song
+			// SongList []*happy.Song
 			Flashes  []interface{}
 		}{
 			localPresenter: &localPresenter{PageTitle: "", PageURL: "", globalPresenter: a.gp},
-			SongList:       songList,
+			// SongList:       songList,
 			Flashes:        fs,
 		}
-		err = a.rndr.HTML(w, http.StatusOK, "index", pp)
+		err := a.rndr.HTML(w, http.StatusOK, "index", pp)
 		if err != nil {
 			a.logr.Log("error when rendering index page: %s", err)
 		}
--- cmd/happyWeb/static/js/index.js
+++ cmd/happyWeb/static/js/index.js
@@ -1,20 +1,109 @@
-$("main").addClass("pre-enter").removeClass("with-hover");
-asd
-setTimeout(function(){
-	$("main").addClass("on-enter");
-}, 500);
-setTimeout(function(){
-	$("main").removeClass("pre-enter on-enter");
-	setTimeout(function(){
-		$("main").addClass("with-hover");
-	}, 50);
-}, 2000);
+var app = function(){
+	$(".back, .header a").click(function(){
+		$(".player").toggleClass("playlist");
+	});
 
-$(".flip, .front a").click(function(){
-	console.log("vo ham` click roi ne`")
-	$(".player").toggleClass("playlist");
-});
+	$("#hamburger-menu").click(function(){
+		$(".player").toggleClass("playlist");
+	});
 
-$(".bottom a").not(".flip").click(function(){
-	$(this).toggleClass("active");
-});
+	var index = 0;
+	var songs = [
+		{
+			provider: "youtube",
+			id: "B7bqAsxee4I"
+		},
+		{
+			provider: "youtube",
+			id: "B7bqAsxee4I"
+		},
+		{
+			provider: "youtube",
+			id: "B7bqAsxee4I"
+		},
+		{
+			provider: "youtube",
+			id: "7dp4GLm7sgo"
+		},
+	];
+	var ytPlayer;
+	var scPlayer;
+	var songEnded = false;
+
+	var onStateChange = function(e){
+		if (e.data === 0) {
+			onSongEnded()
+		}
+	}
+
+	var playNextSong = function() {
+		if (index < songs.length) {
+			index = index+1;
+			playSong(index);
+		}
+	}
+
+	var onSongEnded = function() {
+		console.log("on song ended");
+		if (!songEnded) {
+			songEnded = true;
+			playNextSong();
+		}
+	}
+
+	var playSong = function(index) {
+		console.log(index);
+		if (songs[index].provider == "youtube") {
+			songEnded = false;
+			ytPlayer.loadVideoById(songs[index].id);
+		} else if (songs[index].provider == "soundcloud") {
+			scPlayer.load(songs[index].id, {
+				visual: false,
+				show_artwork: false,
+				auto_play: true,
+				callback: function(){
+					console.log("soundcloud player ready");
+					scPlayer.play();
+					songEnded = false;
+				}
+			})
+		}
+	}
+
+	var waitForYoutube = function(){
+		console.log("wait for youtyube");
+		if (typeof YT === "undefined" || typeof YT.Player === "undefined"){
+			setTimeout(waitForYoutube, 250);
+		} else {
+			console.log("onyoutube ready");
+			ytPlayer = new YT.Player("youtube-player", {
+				events: {
+					'onStateChange': onStateChange,
+					'onReady': function() {
+						console.log("readgy");
+					},
+				}
+			})
+		}
+	}();
+
+	var waitForSoundCloud = function(){
+		if (typeof SC === "undefined"){
+			setTimeout(waitForSoundCloud, 250);
+		} else {
+			console.log("sc ready");
+			scPlayer = SC.Widget("soundcloud-player", {})
+			scPlayer.bind("finish", onSongEnded);
+		}
+	}();
+
+	$("#button-play").click(function(){
+		console.log("called")
+		playSong(index);
+	});
+};
+
+function onYouTubeIframeAPIReady() {
+	app();
+	app = function(){};
+}
